16257480d5ebe9ff919fb903aa98546edc97f1fe
EvgenKost <kost-evgen@mail.ru>
2025-09-27 14:59:31 +0300

    init

---
+++ Services/Worker.cs
@@ -0,0 +1,24 @@
+namespace cache_me_if_you_can.Services;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (_logger.IsEnabled(LogLevel.Information))
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            }
+
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
