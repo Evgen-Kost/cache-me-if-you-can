d17f5e239bb895fcf9980b05cfd997aa9ec2b13a
EvgenKost <kost-evgen@mail.ru>
2025-10-12 13:51:56 +0300

    add SimpleStore Tests

--- Services/SimpleDimpleStore.cs
+++ Services/SimpleDimpleStore.cs
@@ -1,22 +1,91 @@
 namespace cache_me_if_you_can.Services;
 
-public class SimpleDimpleStore
+/// <summary>
+/// SimpleStore
+/// </summary>
+public class SimpleDimpleStore : IDisposable
 {
+    private readonly ReaderWriterLockSlim _lock = new();
     private readonly Dictionary<string, byte[]> _cache = new();
-
-    void Set(string key, byte[] value)
+    private long _setCount;
+    private long _getCount;
+    private long _deleteCount;
+    private bool _disposed;
+    
+    public void Set(string key, byte[] value)
     {
-        _cache[key] = value;
+        try
+        {
+            _lock.EnterWriteLock();
+            _cache[key] = value;
+            
+            Interlocked.Increment(ref _setCount);
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
+    }
+    
+    public byte[]? Get(string key)
+    {
+        try
+        {
+            _lock.EnterReadLock();
+            _cache.TryGetValue(key, out var value);
+            Interlocked.Increment(ref _getCount);
+            return value;
+        }
+        finally
+        {
+            _lock.ExitReadLock();
+        }
+    }
+    
+    public void Delete(string key)
+    {
+        try
+        {
+            _lock.EnterWriteLock();
+            _cache.Remove(key);
+            Interlocked.Increment(ref _deleteCount);
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
+    }
+    
+    public (long setCount, long getCount, long deleteCount) GetStats()
+    {
+        try
+        {
+            _lock.EnterReadLock();
+            return (_setCount, _getCount, _deleteCount);
+        }
+        finally
+        {
+            _lock.ExitReadLock();
+        }
+        
+    }
+    
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this); 
     }
 
-    byte[]? Get(string key)
+    private void Dispose(bool disposing)
     {
-        _cache.TryGetValue(key, out var value);
-        return value;
-    }
+        if (_disposed) return;
 
-    void Delete(string key)
-    {
-        _cache.Remove(key);
+        if (disposing)
+        {
+            _lock.Dispose();
+            _cache.Clear();
+        }
+            
+        _disposed = true;
     }
 }
--- cache-me-if-you-can.Tests/GlobalUsing.cs
+++ cache-me-if-you-can.Tests/GlobalUsing.cs
@@ -1,1 +1,4 @@
 global using cache_me_if_you_can.Services;
+global using cache_me_if_you_can.Extensions;
+
+global using Xunit.Abstractions;
